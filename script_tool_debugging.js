dd9f0545fd3f3e96ba3b3107e95da9bc6320faea
qulbuddin <52233611+qulbuddin@users.noreply.github.com>
2020-09-09 21:47:45 +0700

    Add files via upload

---
+++ script_tool_debugging.js
@@ -0,0 +1,15 @@
+//TOOLS DEBUGGIN JS :
+/*
+console.log()
+alert()
+promt()
+
+example:
+
+var name ="alber"
+console.log(name)
+alert(name)
+
+var name = promt("who are YOU !")
+console.log(name)
+*/
---
+++ script_variable.js
@@ -0,0 +1,24 @@
+//console.log("hello world ! <<-- Script Terpisah")
+//document.getElementById('heading').innerHTML = "Judul Heading"
+//Variabel dalam javascript
+var name = "alber qulbuddin";
+console.log(name);
+
+var lastname="mohammad";
+console.log(lastname);
+
+var age=26;
+console.log(age);
+
+var fullname = name +' '+ lastname;
+console.log(fullname);
+
+var fullname = true
+console.log(fullname)
+
+var job="programmer";
+console.log(job);
+
+var bio = "nama saya " + name + ' ' + lastname + " umur saya " + age + ", pekerjaan saya adalah " + job
+
+console.log(bio);
