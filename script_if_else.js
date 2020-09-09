66f37c3c63c8ce7f8b252b2ce60075086aecedfb
qulbuddin <52233611+qulbuddin@users.noreply.github.com>
2020-09-09 21:46:52 +0700

    Add files via upload

---
+++ script_if_else.js
@@ -0,0 +1,35 @@
+//IF ELSE STATEMENT
+var name1= "Japar"
+var name2 = "Jono"
+var age = 23
+var isYoung = false
+
+
+if(isYoung){
+    console.log(name1 +" Masih Muda")
+}else{
+    console.log(name2 +" Sudah Tua")
+}
+
+if (age < 10){
+    console.log(name1 + " Masih muda")
+}else if (age <= 20){
+    console.log(name2 +" Masih muda")
+}else{
+    console.log(name1 +" & " + name2 + " Sudah Tuek")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script_operator.js
@@ -0,0 +1,54 @@
+var x = 3;
+var y = 10;
+var z = x+y;
+console.log(z);
+console.log(x+y);
+/* jenis jenis operasi
+*
+/
++
+-
+++ --> increment
+-- --> decrement
+=       x=y     x=y
++=      x+=y    x=x+y
+-=      x-=y    x=x-y
+*=      x*=y    x=x*y
+/=      x/=y    x=x/y
+%=      x%=y    x=x%y
+
+*/
+
+//Comparison Operator
+/*
+==      equal to (value)
+===     equeal value and type
+!=      not equal
+>       greather than
+<       less than
+>=
+<=
+?       ternary operator
+*/
+//logival Operator
+/*
+&&      AND
+||      OR
+!       NOT
+*/
+
+//Type Operator
+/*
+typeof
+instanceof
+*/
+//Bitwise
+/*
+&
+|
+-
+^
+<<
+>>
+>>>
+*/
